f0865a6e856e64e505199f95b2b011e9466704b5
DeSertDaXter <marvinwittrock@googlemail.com>
2026-01-22 14:03:59 +0000

    Add files via upload

---
+++ api/auth/login.js
@@ -0,0 +1,29 @@
+module.exports = (req, res) => {
+  const clientId = process.env.TWITCH_CLIENT_ID;
+  const redirectUri = process.env.TWITCH_REDIRECT_URI;
+
+  if (!clientId || !redirectUri) {
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+    return res.end(JSON.stringify({
+      error: "missing_env",
+      missing: {
+        TWITCH_CLIENT_ID: !clientId,
+        TWITCH_REDIRECT_URI: !redirectUri
+      }
+    }));
+  }
+
+  const params = new URLSearchParams({
+    client_id: clientId,
+    redirect_uri: redirectUri,
+    response_type: "code",
+    scope: "moderator:read:followers",
+  });
+
+  const url = `https://id.twitch.tv/oauth2/authorize?${params.toString()}`;
+
+  res.statusCode = 302;
+  res.setHeader("Location", url);
+  res.end();
+};
